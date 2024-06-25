d63d42bca07b566983145cf1c79242dc3547d399
aki_ra <a0323w@gmail.com>
2024-06-25 13:44:20 +0900

    [update] 問題の動きを修正

--- Assets/Shinbo/Scripts/QuestionMove.cs
+++ Assets/Shinbo/Scripts/QuestionMove.cs
@@ -3,39 +3,49 @@ using UnityEngine;
 
 public class QuestionMove : MonoBehaviour
 {
+    [SerializeField]
+    private float _rapidMoveTime = 2f;
+    [SerializeField]
+    private float _finalScale = 2f;
+
+    private float _timer = 0f;
+    private float _solveTime = 0f;
+
     private Transform _tf;
     public static bool IsGameOver;
-    private bool _expansionFlag;
 
     private void Start()
     {
+        _solveTime = GameManager.Instance.ScoreManager.SolveTime;
+
         _tf = gameObject.transform;
         _tf.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         StartCoroutine(Expansion());
     }
 
-    IEnumerator Expansion()
+    private IEnumerator Expansion()
     {
-        while (!_expansionFlag)
+        while (_timer <= _rapidMoveTime)
         {
-            _tf.localScale += new Vector3(0.1f, 0.1f, 0.1f);
-            yield return new WaitForSeconds(0.05f);
+            Debug.Log("rapid");
+            _timer += Time.deltaTime;
+            var elapsed = Time.deltaTime / _rapidMoveTime;
 
-            if (Mathf.Approximately(_tf.localScale.x, 1f)) { _expansionFlag = true; }
+            _tf.localScale += Vector3.one * elapsed;
+            yield return null;
         }
 
-        while (_expansionFlag)
+        while (_timer <= _solveTime)
         {
-            _tf.localScale += new Vector3(0.05f, 0.05f, 0.05f);
-            yield return new WaitForSeconds(0.3f);
+            Debug.Log("solve");
+            var elapsed = Time.deltaTime / _solveTime;
 
-            if (Mathf.Approximately(_tf.localScale.x, 2f))
-            {
-                _expansionFlag = false;
-                Debug.Log("ゲームオーバー");
-                IsGameOver = true;
-                yield break;
-            }
+            _tf.localScale += Vector3.one * elapsed;
+            yield return null;
         }
+
+        Debug.Log("ゲームオーバー");
+        IsGameOver = true;
+        yield break;
     }
 }
