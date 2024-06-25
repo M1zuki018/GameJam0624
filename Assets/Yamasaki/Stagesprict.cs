abcd1112919acd993eb3d7b3335cede8d11a27e1
ymskhrk <ymskhrk0@gmail.com>
2024-06-25 13:30:43 +0900

    ステージ表示するアレ完成

--- Assets/Yamasaki/Stagesprict.cs
+++ Assets/Yamasaki/Stagesprict.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Stagesprict : MonoBehaviour
 {
     [SerializeField] GameObject CountPrefab = default;
+    [SerializeField] GameObject WallPrefab = default;
 
     private GameObject Wall01;
+    private GameObject Walla1;
     // Start is called before the first frame update
     void Start()
     {
         Wall01 = GameObject.Find(CountPrefab.name);
+        Walla1 = GameObject.Find(WallPrefab.name);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("1"))
+        if (Walla1 == false)
         {
             Wall01.SetActive(false);
         }
