0e86489a963456d0559fc7296bc038624ded01c1
ymskhrk <ymskhrk0@gmail.com>
2024-06-25 13:06:24 +0900

    一部スクリプト改善

--- Assets/Yamasaki/Stagesprict.cs
+++ Assets/Yamasaki/Stagesprict.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class Stagesprict : MonoBehaviour
 {
-    [SerializeField] GameObject EnemyPrefab = default;
+    [SerializeField] GameObject CountPrefab = default;
 
+    private GameObject Wall01;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Wall01 = GameObject.Find(CountPrefab.name);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey("1"))
+        {
+            Wall01.SetActive(false);
+        }
+        
     }
-    void OnTriggerEnter(Collider other)
-    {
-        if (EnemyPrefab != null) { Destroy(gameObject); }
-    }
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (EnemyPrefab != null) { Destroy(gameObject); }
+    //}
 }
